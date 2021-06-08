4c0ba8f29f702b81cc9b94558fff8d9d1561d34d
magicbuka <35179123+magicbuka@users.noreply.github.com>
2021-06-08 22:43:45 +0300

    course 3 lesson 2 att 2

--- odometer.py
+++ odometer.py
@@ -1,6 +1,9 @@
 def odometer(oksana):
     res = 0
+    pred_hours = 0
     for i in range(len(oksana)):
         if i%2 == 0:
-            res += oksana[i]
+            current_hours = oksana[i+1]
+            res += (oksana[i] * (current_hours - pred_hours))
+            pred_hours = current_hours
     return res
