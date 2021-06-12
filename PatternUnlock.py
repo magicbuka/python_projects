a1e1879a8d7d8809a88e64e243e134fea169a284
magicbuka <35179123+magicbuka@users.noreply.github.com>
2021-06-12 11:14:51 +0300

    course 3 lesson 6 att 2

--- PatternUnlock.py
+++ PatternUnlock.py
@@ -1,16 +1,15 @@
 def PatternUnlock(N, hits): 
     
-    dd = 2**(1/2)
-    d = {1:1, 3:1, 4:[1, dd], 6:1, 8:1, 2:dd, 5:dd, 7:dd}
+    dd = 2**0.5
+    d = {1:1, 3:1, 4:[1, dd], 6:1, 8:1, 2:dd, 5:[1, dd], 7:dd}
     
     num_res = 0
     for i in range(N-1):
         check = abs(hits[i] - hits[i+1])
-        if check == 4:
-            if hits[i] in [6, 1, 3, 7]:
-                num_res += d[check][1]
-            else:
-                num_res += d[check][0]
+        if (check == 4 and hits[i] in [3, 7]) or (check == 5 and hits[i] in [6, 1]):
+            num_res += d[check][0]
+        elif (check == 4 and hits[i] not in [3, 7]) or (check == 5 and hits[i] not in [6, 1]):
+            num_res += d[check][1]
         else:
             num_res += d[check]
             
