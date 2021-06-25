84be05d70f60ab305c448119ae4638d49a28d69d
magicbuka <35179123+magicbuka@users.noreply.github.com>
2021-06-25 18:35:44 +0300

    course 3 lesson 11 att 3

--- BigMinus.py
+++ BigMinus.py
@@ -6,19 +6,28 @@ def BigMinus(s1, s2):
         max_s = l_1
         lst1 = [int(i) for i in s1]
         lst2 = [int(i) for i in s2.rjust(len(s1), '0')]
-        
     else:
         max_s = l_2
         lst1 = [int(i) for i in s2]
-        lst2 = [int(i) for i in s1.rjust(len(s2), '0')] 
+        lst2 = [int(i) for i in s1.rjust(len(s2), '0')]
 
     res_lst = [0] * max_s
+
+    for i in range(max_s):
+        if lst1[i] == lst2[i]:
+            continue
+        if lst1[i] > lst2[i]:
+            break
+        else:
+            lst1, lst2 = lst2, lst1
+            break
 
     for i in range(max_s - 1, -1, -1):
         num = lst1[i] - lst2[i]
         if num < 0:
             num += 10
             lst1[i - 1] -= 1
+            print(num)
         res_lst[i] = num
 
     if len(res_lst) == 0:
