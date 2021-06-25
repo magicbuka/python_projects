3222004fc22fbce7cbcceb556d99a59845c6be7c
magicbuka <35179123+magicbuka@users.noreply.github.com>
2021-06-25 18:17:35 +0300

    course 3 lesson 11 att 2

--- BigMinus.py
+++ BigMinus.py
@@ -1,15 +1,28 @@
 def BigMinus(s1, s2):
-    if len(s1) > len(s2):
-        max_s = s1
-        min_s = s2
+    l_1 = len(s1)
+    l_2 = len(s2)
+
+    if l_1 > l_2:
+        max_s = l_1
+        lst1 = [int(i) for i in s1]
+        lst2 = [int(i) for i in s2.rjust(len(s1), '0')]
+        
     else:
-        max_s = s2
-        min_s = s1
+        max_s = l_2
+        lst1 = [int(i) for i in s2]
+        lst2 = [int(i) for i in s1.rjust(len(s2), '0')] 
+
+    res_lst = [0] * max_s
+
+    for i in range(max_s - 1, -1, -1):
+        num = lst1[i] - lst2[i]
+        if num < 0:
+            num += 10
+            lst1[i - 1] -= 1
+        res_lst[i] = num
+
+    if len(res_lst) == 0:
+        return '0'
+    else:
+        return str(int(''.join(str(i) for i in res_lst)))
     
-    
-    res = int(max_s[-(len(min_s)):]) - int(min_s)
-    
-    if res < 0:
-        res = ''
-    
-    return max_s[:-(len(min_s))] + str(res)
