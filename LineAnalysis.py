73873f74ddeaca63504c0bf908f23d78d5962b53
magicbuka <35179123+magicbuka@users.noreply.github.com>
2021-07-16 14:25:47 +0300

    course 3 lesson 17 att 2

--- LineAnalysis.py
+++ LineAnalysis.py
@@ -1,20 +1,9 @@
 def LineAnalysis(line):
-    if line[0] == '*' and line[-1] == '*':
-        
-        if len(line)==1:
-            return True
-        
-        if '.' not in line:
-            return True
-        
-        pattern_line = line[0]
-        for i in range(1, len(line)):
-            if line[i] != '*':
-                pattern_line += line[i]
-            else:
-                break
-        
-        line = line.split(pattern_line)
-        str_line = [x for x in line if x != '']
-        
-        return len(str_line) == 1
+
+    lst_line = line.split('*')
+    if len(lst_line) > 1:
+        lst_line = [x for x in lst_line if x != '']
+
+    uniq_elem = set(lst_line)
+
+    return not len(uniq_elem) > 1
