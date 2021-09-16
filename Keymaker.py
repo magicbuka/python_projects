f61fb08a59cbae5bb4512d87aa3bd78743f1b2b1
magicbuka <--replace-all>
2021-09-16 22:20:49 +0300

    course 3 lesson 28 att 1

---
+++ Keymaker.py
@@ -0,0 +1,10 @@
+def Keymaker(k):
+    if k == 1:
+        return '1'
+    
+    line_doors = [0] * k
+    for i in range(1, k+1):
+        if i**2 < k:
+            line_doors[(i**2)-1] = 1
+    
+    return ''.join([str(x) for x in line_doors]) 
