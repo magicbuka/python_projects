c76f05edcb560e3163e4d258b47acb3275a5daff
magicbuka <35179123+magicbuka@users.noreply.github.com>
2022-01-31 17:36:12 +0300

    lesson 13

---
+++ clear_code/variables_time_binding.py
@@ -0,0 +1,37 @@
+# 1. Ранее связывание
+# Задача BastShoe
+# Использованы глобальные переменные для инициализации значений
+class Editor:
+    str = ''
+    history = list()
+    reset = False
+    count = -1
+    last_index = 0
+    undo_lst = list()
+    redo_unable = False
+    
+#...code 
+
+# 2. Во время компиляции
+# Задача Чат на сокетах
+# Информацию о размере буфера, номерах портов и IP-адреса удобнее хранить в качестве констант 
+# для удобства изменения значений и более позднего связывания 
+SIZE_BUFFER = 1024
+SERVER_PORT = 12345
+SERVER_IP = '127.0.0.1'
+
+def check_socket():  
+    while True:
+        data = client.recv(SIZE_BUFFER)
+        #...code 
+
+server_connect = SERVER_IP, SERVER_PORT
+#...code
+
+# 3. Cвязывание во время выполнения программы
+# get_path() это метод, который во время выполнения программы считывает актуальный путь размещения файлов для обработки
+
+path = get_path()
+#...code
+file = open(f'{path}{file_name}.txt', 'r')
+#...code
