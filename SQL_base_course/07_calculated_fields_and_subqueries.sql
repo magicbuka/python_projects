d3dc37b2d8d6e221d48c5cba26c39cbb7a1b42c9
magicbuka <35179123+magicbuka@users.noreply.github.com>
2022-02-27 12:24:10 +0300

    lesson 7

--- SQL_base_course/07_calculated_fields_and_subqueries.sql
+++ SQL_base_course/07_calculated_fields_and_subqueries.sql
@@ -9,15 +9,15 @@ FROM
 SELECT *
 FROM [Order Details]
 WHERE ProductID IN (SELECT ProductID
-					FROM Products
-					WHERE UnitsInStock > 40)
+		    FROM Products
+		    WHERE UnitsInStock > 40)
 
 --7.3.3. 
 SELECT *
 FROM [Order Details]
 WHERE (ProductID IN (SELECT ProductID
-					FROM Products
-					WHERE UnitsInStock > 40))
+		     FROM Products
+		     WHERE UnitsInStock > 40))
 AND (OrderID IN (SELECT OrderID
-				FROM Orders
-				WHERE Freight >= 50))
+	         FROM Orders
+		 WHERE Freight >= 50))
